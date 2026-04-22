168c0d0a13b1bf5464e351830739331d41a63efc
Alexandre Zouiten <alexandre.zouiten1@gmail.com>
2026-04-22 17:26:10 +0200

    implement rational numbers

--- elixir/rational-numbers/lib/rational_numbers.ex
+++ elixir/rational-numbers/lib/rational_numbers.ex
@@ -14,42 +14,50 @@ defmodule RationalNumbers do
   Subtract two rational numbers
   """
   @spec subtract(a :: rational, b :: rational) :: rational
-  def subtract(a, b) do
+  def subtract({a1, b1}, {a2, b2}) do
+    {(a1 * b2 - a2 * b1), (b1 * b2)}
+    |> reduce()
   end
 
   @doc """
   Multiply two rational numbers
   """
   @spec multiply(a :: rational, b :: rational) :: rational
-  def multiply(a, b) do
+  def multiply({a1, b1}, {a2, b2}) do
+    {(a1 * a2), (b1 * b2)}
+    |> reduce()
   end
 
   @doc """
   Divide two rational numbers
   """
   @spec divide_by(num :: rational, den :: rational) :: rational
-  def divide_by(num, den) do
+  def divide_by({a1, b1}, {a2, b2}) do
+    {a1 * b2, a2 * b1}
+    |> reduce()
   end
 
   @doc """
   Absolute value of a rational number
   """
   @spec abs(a :: rational) :: rational
-  def abs(a) do
+  def abs({a, b}) do
+    {Kernel.abs(a), Kernel.abs(b)} |> reduce()
   end
 
   @doc """
   Exponentiation of a rational number by an integer
   """
   @spec pow_rational(a :: rational, n :: integer) :: rational
-  def pow_rational(a, n) do
-  end
+  def pow_rational({a, b}, n) when n < 0, do: {b**Kernel.abs(n), a**Kernel.abs(n)} |> reduce()
+  def pow_rational({a, b}, n), do: {a**n, b**n} |> reduce()
 
   @doc """
   Exponentiation of a real number by a rational number
   """
   @spec pow_real(x :: integer, n :: rational) :: float
-  def pow_real(x, n) do
+  def pow_real(x, {a, b}) do
+    x**(a / b)
   end
 
   @doc """
@@ -58,6 +66,12 @@ defmodule RationalNumbers do
   @spec reduce(a :: rational) :: rational
   def reduce({num, den}) do
     gcd = Integer.gcd(num, den)
-    {num / gcd, den / gcd}
+    num = trunc(num / gcd)
+    den = trunc(den / gcd)
+    if den < 0 do
+      {num * -1, den * -1}
+    else
+      {num, den}
+    end
   end
 end
